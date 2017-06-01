9511d7e782816d783b07c1be3ee1927eb3f36518
Bonnie Porter <bonnieporter@Bonnies-MacBook-Pro-2.local>
2017-06-01 11:08:43 -0400

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Bonnie Porter on 6/1/17.
+//  Copyright © 2017 Bonnie Porter. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
